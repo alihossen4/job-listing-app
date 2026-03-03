029dd5bbe1049fea9b7c0fb8afda30798e595711
alihossen4 <mdahossen.dev@gmail.com>
2026-03-03 04:12:01 -0800

    style: style changes

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 const Cart = ({title, desc}) =>{
     return(
         <>
-        <div className="cart  w-80 h-60 p-30">
+        <div className="cart  w-75 h-60 p-30">
             <img src="" alt="icon" />
 
             <h1 className='text-[25px]'>{title}</h1>
@@ -16,13 +16,12 @@ function Category() {
   return (
     <div className='container mx-auto'>
         <h1 className='text-[45px] my-8 font-[700]'>Explore By <span className='text-[#32a5f8]'>Category</span> </h1>
-        <div className="flex mb-8 mt-15 justify-between">
+        <div className="flex mb-8 mt-15 justify-between grid grid-cols-4 gap-8">
             <Cart title={"Design"} desc={" 235 jobs available"}/>
             <Cart title={"Sales"} desc={" 756 jobs available"}/>
             <Cart title={"Marketing"} desc={" 140 jobs available"}/>
             <Cart title={"Finance"} desc={" 325 jobs available"}/>
-        </div>
-        <div className="flex gap-18 justify-between">
+        
             <Cart title={"Tecknology"} desc={" 235 jobs available"}/>
             <Cart title={"Engineering"} desc={" 756 jobs available"}/>
             <Cart title={"Business"} desc={" 140 jobs available"}/>
--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -3,14 +3,14 @@ import dashboard from "../assets/dashboard.png"
 function Dashboard() {
   return (
     <div className='container '>
-        <div className="dashboard bg-[#4640de] mt-10 flex justify-between">
-            <div className="left w-100 mt-[10%] ml-[8%]">
+        <div className="dashboard bg-[#4640de] mt-10 flex justify-between ">
+            <div className="left w-100 mt-[6%] ml-[8%] my-10">
                 <h1 className='text-[70px] text-[#fff] tracking-tighter font-[600] leading-tight mb-4'>Start Posting jobs today</h1>
                 <p className='text-[#fff] my-2 text-xl'>Start posting jobs for only 10$</p>
                 <button className='btn dashboard-btn bg-[#f0f0f0] text-[#4640de] p-6'>Sign Up for Free</button>
             </div>
-            <div className="right mr-18 mt-15">
-                <img src ={dashboard} alt="dashboard" />
+            <div className="right mr-18 mt-15 mb-5">
+                <img src ={dashboard} alt="dashboard"/>
             </div>
         </div>
     </div>
